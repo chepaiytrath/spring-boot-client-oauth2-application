4b6438f1be7869a080bd99d67f5deacb5003630e
Jatin <jatin.shashoo@gmail.com>
2020-03-02 02:42:56 +0530

    Changed package name for @Test because of spring version change

--- src/test/java/com/example/springbootclientoauth2application/SpringBootClientOauth2ApplicationTests.java
+++ src/test/java/com/example/springbootclientoauth2application/SpringBootClientOauth2ApplicationTests.java
@@ -1,6 +1,7 @@
 package com.example.springbootclientoauth2application;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
