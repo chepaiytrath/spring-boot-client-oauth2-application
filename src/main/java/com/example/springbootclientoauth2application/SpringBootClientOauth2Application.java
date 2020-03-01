07cac1e6be523314056f5bf3874b99279a56d52e
Jatin <jatin.shashoo@gmail.com>
2020-03-02 01:36:06 +0530

    First Commit

---
+++ src/main/java/com/example/springbootclientoauth2application/SpringBootClientOauth2Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootclientoauth2application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootClientOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootClientOauth2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootclientoauth2application/SpringBootClientOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootclientoauth2application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootClientOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
